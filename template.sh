acd68212c2dd535f9d7f53453334faed5addf614
yureedullah <yureedbaloch@gmail.com>
2025-11-03 15:25:37 +0500

    folder strucure addede

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and Files created successfully!"
